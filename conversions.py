4752a99738e3a497f2cc09b1ddf00dcbd2adddcb
Henrik Hamilton <henrikham14@gmail.com>
2024-07-26 11:37:55 -0400

    changed conversions

--- conversions.py
+++ conversions.py
@@ -1,5 +1,5 @@
 def conversion1500mto800m(time):
-    totalseconds = time * 0.492
+    totalseconds = time * 0.4907
     minutes = int(totalseconds // 60)
     seconds = ((totalseconds / 60) - minutes)*60
     seconds = round(seconds, 2)
--- gui.py
+++ gui.py
@@ -2,10 +2,12 @@ from tkinter import *
 from tkinter import ttk
 
 #I copied the basis for this code from the tkinter documentation and have no idea how to use this
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Welcome. This program will show you equivalent performances to any given race time.").grid(column=0, row=0)
-ttk.Button(frm, text="Choose distance")
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=1)
-root.mainloop()
+def openwindow():
+    root = Tk()
+    frm = ttk.Frame(root, padding=10)
+    frm.grid()
+    ttk.Label(frm, text="Welcome. This program will show you equivalent performances to any given race time.").grid(column=0, row=0)
+    ttk.Button(frm, text="Choose distance") #haven't implemented drop-down menu yet but once an option is clicked this a new interface 
+                                            #should show up that allows the user to enter their time
+    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=1)
+    root.mainloop()
