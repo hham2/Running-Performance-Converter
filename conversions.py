1fa378e4ae13d14d9ff0e39df443db7bc16dcee2
Henrik Hamilton <henrikham14@gmail.com>
2024-07-26 23:03:10 -0400

    Worked on conversions and seconds to minutes : seconds formatting. Gonna try to create a piecewise function that represents conversions instead of hard-coding the conversions into the program

--- conversions.py
+++ conversions.py
@@ -1,4 +1,42 @@
-def conversion1500mto800m(time):
+def conversionhub(time, distance):
+    """
+    converts user-input time to an equivalent 1500m time, from 
+    which all other conversions will be made so as to not have
+    to make functions for every possible combination of input 
+    distances and output distances
+    """
+    #temp code begins
+    print(time_format(time))
+    #temp code ends
+    if distance == "800m":
+        time = time * 2.038
+    elif distance == "1600m":
+        time = time * 0.934
+    elif distance == "3000m":
+        time = time * 0.463
+    elif distance == "3200m":
+        time = time * 0.434
+    elif distance == "5000m":
+        if 900 < time <= 940:
+            time = ((time*0.262)+(time*0.264))/2
+        elif 840 < time <= 900:
+            time = time * 0.264
+        elif 800 < time <= 840:
+            time = ((time*0.271)+(time*0.264))/2 #maybe instead of repeatedly adjusting the code here I can use some sort of a function generator to get this working
+                                                 #will probably just test a piecewise function in Desmos 
+        elif 780 < time <= 800:
+            time = time * 0.271
+        else:
+            time = time * 0.262
+    # elif distance == "10000m":
+    # elif distance == "half-marathon":
+    # elif distance == "marathon":
+    return time
+
+
+
+def convert1500mto800m(time):
+    """converts 1500m time to 800m time"""
     totalseconds = time * 0.4907
     minutes = int(totalseconds // 60)
     seconds = ((totalseconds / 60) - minutes)*60
@@ -6,4 +44,24 @@ def conversion1500mto800m(time):
     print(f"{minutes}:{seconds}")
 
 #Jakob Kingebrigtsen
-conversion1500mto800m(206.73)
+# convert1500mto800m(206.73)
+
+def time_format(time):
+    minutes = int(time // 60)
+    seconds = ((time / 60) - minutes)*60
+    seconds = round(seconds, 2)
+    strseconds = str(seconds)
+    if seconds == 0:
+        strseconds = "00.00"
+    elif True:
+        if seconds < 10:
+            strseconds = "0" + strseconds 
+            if "." in strseconds[-2:]:
+                strseconds = strseconds + "0"
+        elif "." in strseconds[-2:]:
+            strseconds = strseconds + "0"
+    result = f"{minutes}:{strseconds}"
+    return result
+
+print(conversionhub(900, "5000m"))
+# print(time_format(480))
--- gui.py
+++ gui.py
@@ -7,7 +7,7 @@ def openwindow():
     frm = ttk.Frame(root, padding=10)
     frm.grid()
     ttk.Label(frm, text="Welcome. This program will show you equivalent performances to any given race time.").grid(column=0, row=0)
-    ttk.Button(frm, text="Choose distance") #haven't implemented drop-down menu yet but once an option is clicked this a new interface 
+    ttk.Button(frm, text="Choose distance") #haven't implemented drop-down menu yet but once an option is clicked a new interface 
                                             #should show up that allows the user to enter their time
     ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=1)
     root.mainloop()
